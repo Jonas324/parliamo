7e36ac3bc4fcae6191d57b8e0b8b2aed3ac8c08b
Jonas324 <jonaswadin24@gmail.com>
2023-01-31 13:18:53 +0100

    made the password input hidden in lagin page

--- parliamo/src/pages/Login.js
+++ parliamo/src/pages/Login.js
@@ -12,7 +12,7 @@ export const Login = (props) => {
             <label htmlFor="username">username</label>
             <input type="username" onChange={(e) => setUsername(e.target.value)} placeholder="write username" id="username" name="username"/>
             <label htmlFor="password">password</label>
-            <input type="passsword" onChange={(e) => setPassword(e.target.value)} placeholder="write password" id="password" name="password"/>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*******"/>
             <button>Log in</button>
         </form>
         <a href="/register">Registrera</a>
